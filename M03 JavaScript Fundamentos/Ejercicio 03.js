341be6316ef7024efcf625c46b223175b58fb4d4
Jonathan Guajardo <jonycioprogramador@gmail.com>
2023-10-03 18:17:47 -0300

    primer intento fallido de ejercicios modulo 2

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,52 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   sonIguales = x == y 
+   return sonIguales
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   tienenMismaLongitud = str1 === str2
+   return tienenMismaLongitud
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   menosQueNoventa = 92
+   if (menosQueNoventa < 90 ) 
+      return true
+   else return false
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   mayorQueCincuenta = 100
+   if (mayorQueCincuenta > 50  )
+       return true
+else return false
 }
+   
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 2 === 0
 }
+
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 2 === 1
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,46 +8,78 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
+   esPositivo = 7
+   if (num >= 1) {
+      console.log("es positivo")
+      return ("es positivo")
+   } else if (num <= -1) {
+      return ("es negativo")
+   } else return false
 }
 
 function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
+var Simbolo = str + "!"
+   agregarSimboloExclamacion = "str"
+   return Simbolo
 }
 
 function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
+var cambiarNombre = nombre + " " + apellido
+   combinarNombres = ( "Jonathan", "Guajardo")
+   return cambiarNombre
 }
 
 function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
+var saludo = "Hola " + (nombre)
+   obtenerSaludo = ("jonathan"); {
+      return saludo
+   }
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
-}
+   area = alto * ancho
+   obtenerAreaRectangulo = (9 * 15); {
+      return area
+   }
+   }
+
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+var perimetro = lado * 4; {
+      return perimetro
 }
-
+}
 function areaDelTriangulo(base, altura) {
    // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
+var area = base * altura / 2; {
+   return area
+}
+
 }
 
 function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
-   // Tu código:
+   // Tu código
+var valorDolar = euro * 1.2; 
+   euro = 5; {
+      return valorDolar
+   }
 }
 
 function esVocal(letra) {
@@ -55,7 +87,18 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
-}
+var laLetraEs = "e"
+letra = (laLetraEs)
+   if (letra.length !== 1); {
+      return " dato incorrecto"
+   }
+letra = letra.toLowerCase();
+
+   if (letra === "a" || letra === "e" || letra === "i"  || letra === "o" || letra === "u") {
+      return "es una vocal"
+
+   } else return " dato incorrecto"
+} 
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
